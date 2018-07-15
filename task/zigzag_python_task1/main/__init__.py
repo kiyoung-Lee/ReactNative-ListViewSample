f7c03e10cf428cb71e8edd0d592c5617bb242d67
KiyoungLee <eevee300@gmail.com>
2018-07-15 15:33:54 +0900

    필터링 작업중

--- task/zigzag_python_task1/main/__init__.py
+++ task/zigzag_python_task1/main/__init__.py
@@ -1,33 +1,50 @@
-import requests
-import json
 from bs4 import BeautifulSoup
+from main.loader import config_loader
+from main.parser import parser
 
-from main.parser import parse
-
-
-def get_html(url):
-    _html = ""
-    resp = requests.get(url)
-    if resp.status_code == 200:
-        _html = resp.text
-    return _html
-
-
-with open('config.json', 'r') as f:
-    config = json.load(f)
-
+# target_name = "66girls"
 target_name = "naning9"
+# target_name = "mocobling"
 
-try:
-    target_mall = config[target_name]
-    target_url = target_mall["url"]
+config = config_loader.load(target_name)
 
-    html = get_html(target_url)
-    soup = BeautifulSoup(html, 'html.parser')
+def six_girls_paser(item_list):
+    for item in item_list:
+        # item_code
+        item_code = item.code
+        split_code = item_code.split('_')
+        item.code = split_code[1]
 
-    item_info_list = parse(target_mall, soup)
+        # item_detail_view_url
+        item_detail_view_url = item.detail_view_url
+        item.detail_view_url = 'www.66girls.co.kr/' + item_detail_view_url
 
-except KeyError:
-    print("Invalid Shopping Mall")
+        item_image_url = item.item_image_url
+        item.item_image_url = item_image_url.strip('//')
+
+def naning_nine_parser(item_list):
+    for item in item_list:
+        # item_code
+        item_code = item.code
+        result = item_code.rfind('index_no')
+        print("result")
+
+if config is not None:
+    try:
+        target_mall = config[target_name]
+        target_url = target_mall["url"]
+
+        parser = parser()
+
+        html = parser.get_html(target_url)
+        soup = BeautifulSoup(html, 'html.parser')
+
+        item_info_list = parser.parse(target_mall, soup)
+        naning_nine_parser(item_info_list)
+    except KeyError:
+        print("Invalid Shopping Mall")
+
+
+
 
 print("end")
--- task/zigzag_python_task1/main/parser.py
+++ task/zigzag_python_task1/main/parser.py
@@ -1,79 +1,86 @@
+import requests
 from main.item import item_info
 
 
-def parse(target_mall, soup):
-    root_tag_list = target_mall["root_tag_list"]
+class parser:
+    def get_html(self, url):
+        _html = ""
+        resp = requests.get(url)
+        if resp.status_code == 200:
+            _html = resp.text
+        return _html
 
-    item_info_list = list()
+    def parse(self, target_mall, soup):
+        root_tag_list = target_mall["root_tag_list"]
 
-    for root_tag in root_tag_list:
-        is_container = root_tag["is_container"]
-        item_code_pattern_key = root_tag["item_code_pattern"]
-        item_detail_url_key = root_tag["item_detail_url_pattern"]
-        image_url_key = root_tag["image_url_pattern"]
-        item_name_key = root_tag["item_name_pattern"]
-        item_price_key = root_tag["item_price_pattern"]
+        item_info_list = list()
 
-        cell_info = target_mall["cell_name"]
-        cell_tag_name = cell_info["tag"]
-        cell_class = cell_info["class"]
+        for root_tag in root_tag_list:
+            is_container = root_tag["is_container"]
+            item_code_pattern_key = root_tag["item_code_pattern"]
+            item_detail_url_key = root_tag["item_detail_url_pattern"]
+            image_url_key = root_tag["image_url_pattern"]
+            item_name_key = root_tag["item_name_pattern"]
+            item_price_key = root_tag["item_price_pattern"]
 
-        if is_container:
-            root_tag_name = root_tag["tag"]
-            root_tag_class = root_tag["class"]
-            item_container = soup.find(root_tag_name, root_tag_class)
+            cell_info = target_mall["cell_name"]
+            cell_tag_name = cell_info["tag"]
+            cell_class = cell_info["class"]
 
-            grid_name_key = root_tag["grid_name_key"]
+            if is_container:
+                root_tag_name = root_tag["tag"]
+                root_tag_class = root_tag["class"]
+                item_container = soup.find(root_tag_name, root_tag_class)
 
-            grid_info = target_mall[grid_name_key]
-            grid_name = grid_info["tag"]
-            grid_class = grid_info["class"]
+                grid_name_key = root_tag["grid_name_key"]
 
-            grid = item_container.find(grid_name, grid_class)
-            cell_list = grid.find_all(cell_tag_name, cell_class)
-        else:
-            cell_list = soup.find_all(cell_tag_name, cell_class)
+                grid_info = target_mall[grid_name_key]
+                grid_name = grid_info["tag"]
+                grid_class = grid_info["class"]
 
-        for item_cell in cell_list:
-            pattern = target_mall["pattern"]
+                grid = item_container.find(grid_name, grid_class)
+                cell_list = grid.find_all(cell_tag_name, cell_class)
+            else:
+                cell_list = soup.find_all(cell_tag_name, cell_class)
 
-            item_code = get_item_by_pattern(item_cell, pattern[item_code_pattern_key])
-            item_detail_url = get_item_by_pattern(item_cell, pattern[item_detail_url_key])
-            item_image_url = get_item_by_pattern(item_cell, pattern[image_url_key])
-            item_name = get_item_by_pattern(item_cell, pattern[item_name_key])
-            item_price = get_item_by_pattern(item_cell, pattern[item_price_key])
+            for item_cell in cell_list:
+                pattern = target_mall["pattern"]
 
-            item = item_info(item_code, item_detail_url, item_image_url, item_name, item_price)
-            item_info_list.append(item)
+                item_code = self.get_item_by_pattern(item_cell, pattern[item_code_pattern_key])
+                item_detail_url = self.get_item_by_pattern(item_cell, pattern[item_detail_url_key])
+                item_image_url = self.get_item_by_pattern(item_cell, pattern[image_url_key])
+                item_name = self.get_item_by_pattern(item_cell, pattern[item_name_key])
+                item_price = self.get_item_by_pattern(item_cell, pattern[item_price_key])
 
-    return item_info_list
+                item = item_info(item_code, item_detail_url, item_image_url, item_name, item_price)
+                item_info_list.append(item)
 
+        return item_info_list
 
-def get_item_by_pattern(item_cell, pattern_list):
-    item_result = item_cell
-    pattern_list_length = len(pattern_list)
-    for idx in range(len(pattern_list)):
-        item_result = parser_by_pattern(item_result, pattern_list[idx])
+    def get_item_by_pattern(self, item_cell, pattern_list):
+        item_result = item_cell
+        pattern_list_length = len(pattern_list)
+        for idx in range(len(pattern_list)):
+            item_result = self.parser_by_pattern(item_result, pattern_list[idx])
 
-    if (idx + 1) == pattern_list_length:
-        return item_result
+        if (idx + 1) == pattern_list_length:
+            return item_result.encode('utf-8')
 
+    def parser_by_pattern(self, target, pattern):
+        command = pattern["command"]
+        tag_name = pattern["tag"]
+        if "class" in pattern:
+            class_name = pattern["class"]
 
-def parser_by_pattern(target, pattern):
-    command = pattern["command"]
-    tag_name = pattern["tag"]
-    if "class" in pattern:
-        class_name = pattern["class"]
-
-    if command == "find":
-        return target.find(tag_name)
-    elif command == "find_class":
-        return target.find(tag_name, class_name)
-    elif command == "element":
-        return target[tag_name]
-    elif command == "text":
-        return target.text
-    elif command == "find_all":
-        return target.find_all(tag_name)
-    elif command == "find_all_class":
-        return target.find_all(tag_name, class_name)
+        if command == "find":
+            return target.find(tag_name)
+        elif command == "find_class":
+            return target.find(tag_name, class_name)
+        elif command == "element":
+            return target[tag_name]
+        elif command == "text":
+            return target.text
+        elif command == "find_all":
+            return target.find_all(tag_name)
+        elif command == "find_all_class":
+            return target.find_all(tag_name, class_name)
