6a0b9899f9a8453598b798da59d3e42ca7469782
KiyoungLee <eevee300@gmail.com>
2018-07-17 01:03:19 +0900

    filter 구현

--- task/zigzag_python_task1/main/__init__.py
+++ task/zigzag_python_task1/main/__init__.py
@@ -8,49 +8,6 @@ target_name = "mocobling"
 
 config = config_loader.load(target_name)
 
-def six_girls_paser(item_list):
-    for item in item_list:
-        # item_code
-        item_code = item.code
-        split_code = item_code.split('_')
-        item.code = split_code[1]
-
-        # item_detail_view_url
-        item_detail_view_url = item.detail_view_url
-        item.detail_view_url = 'www.66girls.co.kr/' + item_detail_view_url
-
-        item_image_url = item.item_image_url
-        item.item_image_url = item_image_url.strip('//')
-
-def naning_nine_parser(item_list):
-    for item in item_list:
-        # item_code
-        item_code = item.code
-        item_code_start_pos = item_code.find('index_no')
-        item_code_end_pos = item_code.find('&')
-
-        substring_start_idx = item_code_start_pos + 9
-        item.code = item_code[substring_start_idx:item_code_end_pos]
-
-        # detail_vice_url
-        detail_view_url = item.detail_view_url
-        item.detail_view_url = "http://www.naning9.com" + detail_view_url
-        print("result")
-
-def mocobling_parser(item_list):
-    for item in item_list:
-        # item_code
-
-        # detail_view_url
-        detail_view_url = item.detail_view_url
-        item.detail_view_url = "http://www.mocobling.com" + detail_view_url
-
-        # item_name
-        # 한글주석
-        item_name = item.name
-        item.name = item_name.encode('utf-8').decode('utf-8')
-        print("test")
-
 if config is not None:
     try:
         target_mall = config[target_name]
@@ -62,11 +19,7 @@ if config is not None:
         soup = BeautifulSoup(html, 'html.parser')
 
         item_info_list = parser.parse(target_mall, soup)
-        mocobling_parser(item_info_list)
     except KeyError:
         print("Invalid Shopping Mall")
-
-
-
 
 print("end")
--- task/zigzag_python_task1/main/parser.py
+++ task/zigzag_python_task1/main/parser.py
@@ -1,7 +1,6 @@
 import requests
 from main.item import item_info
 
-
 class parser:
     def get_html(self, url):
         _html = ""
@@ -53,6 +52,7 @@ class parser:
                 item_price = self.get_item_by_pattern(item_cell, pattern[item_price_key])
 
                 item = item_info(item_code, item_detail_url, item_image_url, item_name, item_price)
+                self.filter(target_mall, item)
                 item_info_list.append(item)
 
         return item_info_list
@@ -84,3 +84,62 @@ class parser:
             return target.find_all(tag_name)
         elif command == "find_all_class":
             return target.find_all(tag_name, class_name)
+
+    def filter(self, target_mall, item):
+        filter_contract = target_mall["filter"]
+
+        # item_code
+        if "item_code" in filter_contract:
+            item_code_filter_list = filter_contract["item_code"]
+            item.code = self.get_value_by_filter(item.code, item_code_filter_list)
+
+        # detail_view_url
+        if "detail_view_url" in filter_contract:
+            detail_view_url_filter_list = filter_contract["detail_view_url"]
+            item.detail_view_url = self.get_value_by_filter(item.detail_view_url, detail_view_url_filter_list)
+
+        # item_image_url
+        if "item_image_url" in filter_contract:
+            item_image_url_filter_list = filter_contract["item_image_url"]
+            item.item_image_url = self.get_value_by_filter(item.item_image_url, item_image_url_filter_list)
+
+        # item_name
+        if "item_name" in filter_contract:
+            item_name_filter_list = filter_contract["item_name"]
+            item.name = self.get_value_by_filter(item.name, item_name_filter_list)
+
+
+    def get_value_by_filter(self, value, filter_list):
+        filter_value_result = value
+        result_list = list()
+
+        for filter in filter_list:
+            filter_value_result = self.execute_command_filter(value, filter, result_list)
+            result_list.append(filter_value_result)
+
+        return filter_value_result
+
+    def execute_command_filter(self, value, filter, result_value_list):
+        command = filter["command"]
+        parameter_list = filter["parameters"]
+
+        if command == "find":
+            return value.find(parameter_list[0])
+        elif command == "plus_back_get_result":
+            return result_value_list[parameter_list[0]] + parameter_list[1]
+        elif command == "substring_get_result":
+            start = result_value_list[parameter_list[0]]
+            end = result_value_list[parameter_list[1]]
+            return value[start:end]
+        elif command == "plus_front":
+            return parameter_list[0] + value
+        elif command == "split":
+            return value.split(parameter_list[0])
+        elif command == "element":
+            split_list = result_value_list[parameter_list[0]]
+            return split_list[parameter_list[1]]
+        elif command == "strip":
+            return value.strip(parameter_list[0])
+        elif command == "encode":
+            return value
+
